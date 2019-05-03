2d16216888d44522ca4174e1a3c553a439a79491
HeapsortYi <zhangyi30.happy@gmail.com>
2019-05-03 12:07:06 +0800

    'update'

--- README.md
+++ README.md
@@ -1,4 +1,15 @@
 # PKU_AutoJump
-用于北大食堂”跳一跳“游戏的辅助程序
+## Introduction
+用于北大食堂”跳一跳“游戏的辅助程序。仅限安卓手机，调试平台为Windows 10。
 
-参考了[微信跳一跳辅助程序](https://github.com/wangshub/wechat_jump_game)的部分思路和代码。但是因为截图速率太慢，最多只能跳15次。希望以后能解决这个问题。。。
+参考了[微信跳一跳辅助程序](https://github.com/wangshub/wechat_jump_game)的部分思路和代码。思路为先对游戏界面截屏，然后将图片传到PC端，通过搜索判断下一步应该向左还是向右跳。接着通过adb发出模拟点击命令控制跳动。
+
+尝试了三种截图方法，但是因为速率还是太慢，最多只能跳到21次。。。希望有大佬能解决这个问题
+
+<p align="center"><img src="autojump.jpg" width = "270" height = "480"/></div>
+
+## Requirements
+- 需要下载Adb并配置到系统路径。如果不想下载，可以直接把Tools文件夹下的所有文件复制到根目录也行
+- 截图时使用了[uiautomator2](https://github.com/openatx/uiautomator2)，是三种截图方法里最快的。如果不想使用，可以不安装，设置程序里的screenshot_way=0或1即可
+- 调试时设置手机屏幕长亮
+- 其他import的python库
--- pku_auto_jump.py
+++ pku_auto_jump.py
@@ -1,3 +1,4 @@
+# coding: utf-8
 import os
 import subprocess
 import time
@@ -10,32 +11,43 @@ from PIL import Image
 screenshot_way = 2
 
 
-def pull_screenshot():  # 获取截图
+def pull_screenshot():  # 获取截图。screenshot_way分别对应3种截图方式，从高到低越来越慢
     global screenshot_way
-    if screenshot_way in [1, 2]:
+    if screenshot_way == 2:
+        # 在使用时需要将devices_ip换成自己设备的，可以通过adb devices命令查看
+        os.system(
+            'python -m uiautomator2 screenshot {devices_ip} autojump.jpg'.format(
+                devices_ip='xxxxxxxxxxxx'))
+        return None
+    elif screenshot_way == 1:
         process = subprocess.Popen(
             'adb shell screencap -p', shell=True, stdout=subprocess.PIPE)
         screenshot = process.stdout.read()
-        if screenshot_way == 2:
-            binary_screenshot = screenshot.replace(b'\r\n', b'\n')
-        else:
-            binary_screenshot = screenshot.replace(b'\r\r\n', b'\n')
+        binary_screenshot = screenshot.replace(b'\r\n', b'\n')
+
         return binary_screenshot
     elif screenshot_way == 0:
-        os.system('adb shell screencap -p /sdcard/autojump.png')
-        os.system('adb pull /sdcard/autojump.png .')
+        os.system('adb shell screencap -p /sdcard/autojump.jpg')
+        os.system('adb pull /sdcard/autojump.jpg .')
+        return None
 
 
 def search(img_jump=None):
-    thres1, thres2, thres3 = 255, 210, 62
+    R, G, B = 255, 210, 62
+
     if img_jump is None:
-        img_jump = cv2.imread('autojump.png')
-        thres1, thres3 = thres3, thres1
-    # 在右边搜索
+        img_jump = cv2.imread('autojump.jpg')
+        R, B = B, R
+    else:
+        img_jump = Image.open(BytesIO(img_jump))
+        img_jump = np.array(img_jump)
+
+    # 经过观察发现包子的坐标是固定不变的，那么只要在右边某个固定的范围搜索有没有出现橘色的点即可。
+    # 如果出现，则向右跳；否则，向左跳
     for x in range(1525, 1542):
         for y in range(631, 663):
-            if abs(img_jump[x, y, 0] - thres1) <= 1 and abs(img_jump[x, y, 1] - thres2) <= 1 and abs(
-                    img_jump[x, y, 2] - thres3) <= 1:
+            if abs(img_jump[x, y, 0] - R) <= 2 and abs(img_jump[x, y, 1] - G) <= 2 and abs(
+                    img_jump[x, y, 2] - B) <= 2:
                 return True
     return False
 
@@ -56,14 +68,11 @@ def run():
         count += 1
         print('---\n%-12s %s (%s)' % ('Times:', count, int(time.time())))
         # 获取截图
-        binary_screenshot = pull_screenshot()
-        img = Image.open(BytesIO(binary_screenshot))
-        img = np.array(img)
-
-        inRight = search(img)
+        screenshot = pull_screenshot()
+        inRight = search(screenshot)
         jump(inRight)
 
-        time.sleep(1)
+        # time.sleep(1)
         print('Continue!')
 
 
